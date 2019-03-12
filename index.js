eb0621b556ee33b28175d3c1099531498dc2a1be
Bryce Hoyt <bhoyt@rams.colostate.edu>
2019-03-12 17:42:25 -0600

    adds a run bash script changes port to 1025 for local host

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const port = 8088;
+const port = 1025;
 app.get('/', main);
 function main(req, res){
     res.send("A multitouch paint app.");
