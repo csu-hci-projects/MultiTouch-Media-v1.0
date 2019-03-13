ac1df297e16d7f7c37b7f79963753347e22d72ea
Zack Jones <justzackj@gmail.com>
2019-03-13 09:03:31 -0600

    Rename index.js to server.js, allow it to server js files

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
 
-const port = 1025;
+const port = 8088;
 app.set('view engine', 'pug');
-app.get('/', main);
+app.get('/', main)
+app.use('/js', express.static(__dirname + '/js/'));
 
 function main(req, res){
+
+
     res.render('index');
 }
 
