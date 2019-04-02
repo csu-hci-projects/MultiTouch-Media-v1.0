817baae69253e96a77ad3a82fb2cc7119299cce3
Zack Jones <justzackj@gmail.com>
2019-04-02 12:55:18 -0600

    Add simple animation and open/close functions

--- js/radial.js
+++ js/radial.js
@@ -1,3 +1,10 @@
-function openMenu(){
-    $("touchArea").animate({padding: "100px"});        
+function openMenu(where){ 
+    $("div#radialWrapper").css({top: where.x + "px", left: where.y + "px"});        
+    $("a#touchArea").animate({padding: "100px"});        
+}
+
+function closeMenu(){
+    $("a#touchArea").animate({padding: "0"}, () => {        
+        $("div#radialWrapper").css({top: "-9999px", left: "-9999px"});        
+    });
 }    
