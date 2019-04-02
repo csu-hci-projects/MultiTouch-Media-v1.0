2199b89dac9d92170d87cf8b03abda28781b77b6
Zack Jones <justzackj@gmail.com>
2019-04-02 12:13:35 -0600

    Change Port back

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const port = 8888;
+const port = 8088;
 app.set('view engine', 'pug');
 app.get('/', main)
 app.use('/images', express.static(__dirname + '/images'));
