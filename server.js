2ac71e50c48af6d4144cdc49732f06d2afa05e87
Zack Jones <justzackj@gmail.com>
2019-04-02 12:12:01 -0600

    Added slider to turn touch on/off

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const port = 8088;
+const port = 8888;
 app.set('view engine', 'pug');
 app.get('/', main)
 app.use('/images', express.static(__dirname + '/images'));
@@ -9,9 +9,9 @@ app.use('/js', express.static(__dirname + '/js/'));
 app.use('/css', express.static(__dirname + '/css/'));
 
 function main(req, res){
-
-
-    res.render('index');
+    let doTouch = true;
+    if(req.param('notouch') === '') doTouch = false; 
+    res.render('index', {touch: doTouch});
 }
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
